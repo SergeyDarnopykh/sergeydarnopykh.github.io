c68b91e1269f177897644b9a06e0c2e6f54c2526
SergeyDarnopykh <sergey.darnopyh@gmail.com>
2017-09-27 21:37:58 +0300

    _

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,10 +2,12 @@
 
 require('../stylesheets/reset.less');
 require('../stylesheets/global.less');
+require('../stylesheets/link.less');
 require('../stylesheets/cards.less');
 require('../stylesheets/header.less');
 require('../stylesheets/person.less');
 require('../stylesheets/card.less');
 require('../stylesheets/footer.less');
+require('../stylesheets/mobile.less');
 
 //cd /D E:/Sergey_work/GitHub/sergeydarnopykh.github.com
